43f63b13e8256016db1483ee693e8c8f3366f836
Gaurav Singh <gauravsingh21100@gmail.com>
2023-02-12 01:41:24 +0530

    other routes have added

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello world")
+    res.send("Welcome to mining!!")
 })
+
+app.get('/students',(req,res)=>{
+    res.send("this is student section!!")
+})
+
+app.get('/faculty',(req,res)=>{
+    res.send("this is faculty section")
+})
+
+app.get('/events',(req,res)=>{
+    res.send("this is event section");
+})
+
+
 
 app.listen(3000,()=>{
     console.log("Server Started")
