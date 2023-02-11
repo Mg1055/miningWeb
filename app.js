cf42c49e2ee433d73ba362b7b08ea31f192b23f5
Gaurav Singh <gauravsingh21100@gmail.com>
2023-02-12 02:02:34 +0530

    Adding ejs

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
-const express=require('express');
-const app=express();
-
-app.get('/',(req,res)=>{
-    res.send("Welcome to mining!!")
-})
-
-app.get('/students',(req,res)=>{
-    res.send("this is student section!!")
-})
-
-app.get('/faculty',(req,res)=>{
-    res.send("this is faculty section")
-})
-
-app.get('/events',(req,res)=>{
-    res.send("this is event section");
-})
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Packages>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const express = require("express");
+const app = express();
+const path=require('path')
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Packages>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 
 
-app.listen(3000,()=>{
-    console.log("Server Started")
-})
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Middleware>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+app.set("views", path.join(__dirname, "views"));
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Middleware>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<ROUTES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+app.get("/", (req, res) => {
+  res.send("Welcome to mining!!");
+});
+
+app.get("/students", (req, res) => {
+  res.send("this is student section!!");
+});
+
+app.get("/faculty", (req, res) => {
+  res.send("this is faculty section");
+});
+
+app.get("/events", (req, res) => {
+  res.send("this is event section");
+});
+
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<ROUTES>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+app.listen(3000, () => {
+  console.log("Server Started");
+});
