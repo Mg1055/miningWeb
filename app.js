dbed41881958f081beacbf09d2c2ae8b101b83a2
Gaurav Singh <gauravsingh21100@gmail.com>
2023-02-15 02:24:03 +0530

    adding cards in faculty section

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const path=require('path')
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Middleware>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 app.set("view engine","ejs")
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static("views"));
 app.use(express.urlencoded({extended:true}));
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Middleware>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
